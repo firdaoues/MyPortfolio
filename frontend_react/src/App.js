ca0b2c946316e9b24fb23420eb30d8be4749dcae
firdaoues <firdaouesmiraoui@gmail.com>
2022-08-01 23:50:28 +0200

    Initialization

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 
 import { About, Header, Footer, Skills, Work, Testimonial } from './container';
 import { Navbar } from './components';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Navbar />
         <Header />
         <About />
         <Work />
+        <Skills />
         <Testimonial />
         <Footer />
 
