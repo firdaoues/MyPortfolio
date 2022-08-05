5708780520150480c66e4c6261269af4e53681aa
firdaoues <firdaouesmiraoui@gmail.com>
2022-08-05 03:01:40 +0200

    work

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import {AiFillEye, AiFillGithub} from 'react-icons/ai';
+import { motion } from 'framer-motion';
+import AppWrap from '../../wrapper/AppWrap';
 
 import './Work.scss';
 
