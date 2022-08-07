74c659dc0c1f2043c15d548dda88d2835d3ab52c
firdaoues <firdaouesmiraoui@gmail.com>
2022-08-08 01:50:54 +0200

    skills

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -33,10 +33,22 @@ const Skills = () => {
   return (
    <>
    <h2 className="head-text">Skills & Experience</h2> 
-    <div className="app__skills-container">
 
+
+    <div className="app__skills-container">
       <motion.div className="app__skills-list">
-         
+         {skills.map((skill) => (
+          <motion.div 
+          whileInView ={{opacity: [0, 1]  }}
+          transition = {{ duration: 0.5 }}
+          className="app__skills-item app__flex"
+          key = {skill.name} >
+            <div className="app__flex" style= {{ backgroundColor: skill.bgColor}}>
+              <img src={urlFor(skill.icon)} alt={skill.name} />
+              <p className="p-text">{skill.name}</p>
+            </div>
+          </motion.div>
+         ))}
 
       </motion.div>
     </div>
