d09e6e17794ba464276d55d44da8388fc3ad1f37
firdaoues <firdaouesmiraoui@gmail.com>
2022-08-07 16:15:41 +0200

    work scss

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -37,7 +37,7 @@ const Work = () => {
           <div 
           key = {index}
           onClick={() => handleWorkFilter(item)} 
-          className={`app__work-filter app__flex p-text ${activeFilter === item ? 'item-active' : ''}`} > {item}
+          className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`} > {item}
           </div>
          
         ))}
