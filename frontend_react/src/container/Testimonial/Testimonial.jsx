10c435788b65a691ffed28206913e4670e04404f
firdaoues <firdaouesmiraoui@gmail.com>
2022-08-12 00:10:52 +0200

    brands

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -65,7 +65,15 @@ const Testimonial = () => {
       </>
     )}
     
-    <div className="app__flex">
+    <div className="app__testimonials-brands app__flex">
+      {brands.map((brand) => (
+        <motion.div 
+        whileInView={{opacity: [0, 1]  }}
+        transition={{ duration: 0.5, typr:'tween'}}
+        key={brand._id} >
+          <img src={urlFor(brand.imgUrl)} alt={brand.name} />
+        </motion.div>
+      ))}
     </div>
     </>
   )
