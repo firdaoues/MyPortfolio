c14aa8dc32dd0f65c22792184e4b8a5c430c6990
firdaoues <firdaouesmiraoui@gmail.com>
2022-08-11 23:05:12 +0200

    test

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -32,8 +32,36 @@ const Testimonial = () => {
         })
     
 }, []);
+
+  const test = testimonials[currentIndex];
+  const handleClick =(index) => {
+    setCurrentIndex(index);
+  }
+
   return (
-    <div>Testimonial</div>
+    <>
+    {testimonials.length && (
+      <>
+      <div className="app__testimonials-item app__flex ">
+        <img src={urlFor(test.imgUrl)} alt="testimonial" />
+        <div className="app__testimonials-content">
+          <p className="p-text">{test.feedback}</p>
+        <div>
+          <h4 className="bold-text">{test.name}</h4>
+          <h5 className="p-text">{test.company}</h5>
+        </div>
+        </div>
+      </div>
+
+      <div className="app__testimonials-btns app__flex ">
+        <div className="app__flex" onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}></div>
+      <HiChevronLeft />
+      <div className="app__flex" onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}></div>
+      <HiChevronRight />
+      </div>
+      </>
+    )}
+    </>
   )
 }
 
