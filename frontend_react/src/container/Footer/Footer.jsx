b3cd8eaaa94e5d8a4c8ec33e4bbacca3e9ca28e9
firdaoues <firdaouesmiraoui@gmail.com>
2022-08-23 00:22:56 +0200

    form

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -9,8 +9,35 @@ import './Footer.scss';
 
 
 const Footer = () => {
+   const [formData, setFormData] = useState({name :'', email :'', message :''})
+   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+   const [loding, setLoding] = useState(false);
   return (
-    <div>Footer</div>
+   <>
+   <h2 className='head-text'> Take a coffe and chat with me</h2>
+   <div className='app__footer-cards'>
+    <div className='app__footer-card'>
+      <img src = {images.email} alt ="email" />
+      <a href="mailto:firdaouesmiraoui@gmail.com" className="p-text">firdaouesmir@gmail.com</a>
+    </div>
+    <div className='app__footer-card'>
+      <img src = {images.mobile} alt ="mobile" />
+      <a href="tel: 1234567" className="p-text">1234567</a>
+    </div>
+   </div>
+   <div className='app__footer-form app__flex'>
+    <div className='app__flex'>
+      <input className='p-text' type= 'text' placeholder='Your Name' name='name' value={name} onChange={handleChangeInput}/>
+    </div>
+    <div className='app__flex'>
+      <input className='p-text' type= 'text' placeholder='Your Email' name='email' value={email} onChange={handleChangeInput}/>
+    </div>
+    <div>
+      <textarea className='p-text' placeholder='Your message' value={message} name={message} onChange={handleChangeInput}/>
+    </div>
+    <button type='button' className='p-text' onChange={handleChangeInput}>Send Message</button>
+   </div>
+   </>
   )
 }
 
